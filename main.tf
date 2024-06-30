40186f74ace48aef50e4751bd2cba5de1b141ae1
root <root@DESKTOP-8FVQ4BQ>
2024-06-30 16:14:36 +0530

    {added files for resource file }

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+       required_providers {
+           aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+   required_version = ">= 1.2.0"
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+ }
+
+
+# Create a VPC
+resource "aws_instance" "app_server" {
+     ami    = "ami-830c94e3"
+     instance_type = "t2.micro"
+    tags = {
+           Name = "terraform_demo"
+
+}
+}
